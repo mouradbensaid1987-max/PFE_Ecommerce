f9c95c05ca2cbd09cfff84b1f7269ad4f60d0747
BEN SAID Mourad <mourad.bensaid.1987@gmail.com>
2026-07-03 23:42:17 +0200

    Tâche 11 — Fixtures : 6 catégoriesde démo

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,13 +2,37 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\String\Slugger\AsciiSlugger;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+
+      $slugger = new AsciiSlugger();
+
+      $categories = [
+        'Informatique',
+        'Téléphonie',
+        'Vêtements',
+        'Maison',
+        'Sports',
+        'Livres',
+        ];
+
+        foreach ($categories as $name) 
+          {
+              $cat = new Category();
+              $cat->setName($name);
+              $cat->setSlug(strtolower($slugger->slug($name)));
+              $cat->setDescription('Description de la catégorie ' . $name);
+              $manager->persist($cat);
+          }
+
+
         // $product = new Product();
         // $manager->persist($product);
 
