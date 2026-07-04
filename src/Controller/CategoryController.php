656c5882d7682874b08cd55903eeb34cb3005398
BEN SAID Mourad <mourad.bensaid.1987@gmail.com>
2026-07-05 01:24:01 +0200

    Tâche 13 — Ajouter le menu'Catégories' dans la navbar (dropdown)

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CategoryController extends AbstractController
+{
+    #[Route('/category', name: 'app_category')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+
+        return $this->render('/templates/base.html.twig', [
+            'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+}
