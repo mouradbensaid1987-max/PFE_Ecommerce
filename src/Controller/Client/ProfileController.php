c3cfe307972f7156a8e519e838c4e5126996dd09
BEN SAID Mourad <mourad.bensaid.1987@gmail.com>
2026-07-13 11:47:04 +0200

    Tâche 28 — Modifier son profil

--- src/Controller/Client/ProfileController.php
+++ src/Controller/Client/ProfileController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Client; 
 
+use App\Form\ProfileFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -19,6 +22,24 @@ class ProfileController extends AbstractController
     return $this->render('profile/show.html.twig', [
     'user' => $this->getUser(),
     ]);
+  }
+
+
+
+  #[Route('/edit', name: 'app_profile_edit')]
+  public function edit(Request $request, EntityManagerInterface $em): Response
+  {
+      $form = $this->createForm(ProfileFormType::class, $this->getUser());
+      $form->handleRequest($request);
+      if ($form->isSubmitted() && $form->isValid()) 
+        {
+            $em->flush();
+            $this->addFlash('success', 'Profil mis à jour avec succès !');
+            return $this->redirectToRoute('app_profile');
+        }
+      return $this->render('profile/edit.html.twig', [
+          'form' => $form->createView(),
+        ]);
   }
 
 }
